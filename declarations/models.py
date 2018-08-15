a87f16ae2e0868f893b74e8cf1c2ce04a1e60820
Ravis <aao.t@yandex.ru>
2018-08-15 15:46:03 +0300

    style improvements

--- declarations/models.py
+++ declarations/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.db.models import ForeignKey
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from mptt.models import MPTTModel, TreeForeignKey
+
+year_list = []
 
 
 class Office(MPTTModel):
@@ -10,11 +14,33 @@ class Office(MPTTModel):
         default=0, verbose_name='Порядок сортировки'
     )
 
+    def get_status(self):
+        result = []
+        for year in year_list:
+            file_count = DocumentFile.objects.filter(
+                document__income_year=year,
+                document__office__id=self.id,
+                file__isnull=False
+            ).count()
+            declaration_count = Document.objects.filter(
+                office__id=self.id, income_year=year
+            ).count()
+            if file_count > 0 and file_count == declaration_count:
+                result.append('all_files')
+            elif file_count > 0 and file_count != declaration_count:
+                result.append('some_files')
+            elif file_count == 0 and declaration_count > 0:
+                result.append('no_files_but_declaration')
+            elif file_count == 0 and declaration_count == 0:
+                result.append('nothing')
+        return result
+
     def __str__(self):
         return self.name
 
 
 class Document(models.Model):
+
     office = ForeignKey(
         'declarations.Office', verbose_name="орган власти",
         on_delete=True, related_name='documents'
@@ -27,10 +53,27 @@ class Document(models.Model):
         return 'Документ из организации ' + self.office.name
 
 
+@receiver(post_save, sender=Document)
+def call_refres_year_list(sender, **kwargs):
+    refres_year_list()
+
+
+def refres_year_list():
+    year_list = Document.objects.all().distinct(). \
+        order_by('-income_year').values_list('income_year', flat=True)
+    if len(year_list) == 0:
+        return []
+    return list(range(year_list[0], year_list[len(year_list)-1]-1, -1)) if len(year_list) > 1 else [year_list[0]]
+
+
+year_list = refres_year_list()
+
+
 class DocumentFile(models.Model):
     document = models.ForeignKey(
         Document, verbose_name="декларация",
-        on_delete=True
+        on_delete=True,
+        related_name='files'
     )
     file = models.FileField(
         blank=True, max_length=255, null=True,
--- declarations/views.py
+++ declarations/views.py
@@ -1,22 +1,17 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
-from declarations.models import Office, Document
+from declarations.models import Office, year_list
 
 
 class DeclarationListView(ListView):
     template_name = 'declaration_list_view.html'
 
     def get_queryset(self):
-        return Office.objects.get(id=self.kwargs['id']).get_ancestors()
+        return Office.objects.get(id=self.kwargs['id']).get_children()
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['office_list'] = Office.objects.get(
-            id=int(self.kwargs['id'])
-        ).get_children()
-        context['years'] = Document.objects.all().order_by('-income_year')\
-            .distinct().values_list('income_year', flat=True)
+        context['years'] = year_list
         return context
 
     class Meta:
